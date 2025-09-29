b4bd73daa5f766e07a0e7bcc6c67b8fa27c69ac3
Eiji Alford <eijialford@gmail.com>
2025-09-29 14:09:53 -0700

    Added the initial python file

---
+++ Camera_Facing_Normal.py
@@ -0,0 +1,21 @@
+bl_info = {
+    "name": "Camera Facing Normal",
+    "author": "Lambdah",
+    "version": (0,0,1),
+    "blender": (4, 0, 0),
+    "location": "View3D > Sidebar > Camera Facing Normal",
+    "description": "Takes a rendered normal map facing towards the camera",
+    "doc_url": "https://github.com/Lambdah/Camera_Facing_Normal",
+    "category": "Renders"
+}
+
+import bpy
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
